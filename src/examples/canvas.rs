85d8c116357944e0b47fe7f222d82a05fd9e7e2a
Mark LeMoine <thatsgobbles@gmail.com>
2019-06-21 17:02:34 -0700

    Working on making gradient lines as nice code-wise as possible

--- src/examples/canvas.rs
+++ src/examples/canvas.rs
@@ -7,6 +7,9 @@ use cursive::views::Canvas;
 use cursive::views::Dialog;
 use cursive::traits::Scrollable;
 use cursive::theme::Color;
+use cursive::theme::ColorType;
+use cursive::theme::ColorStyle;
+use cursive::theme::PaletteColor;
 
 // const BLOCK_B_0_8: char = ' ';
 // const BLOCK_B_1_8: char = '▁';
@@ -62,6 +65,276 @@ fn gen_l_r_bar(logical_len: usize) -> Vec<char> {
     chars.push(tail_char);
 
     chars
+}
+
+type RGB = (u8, u8, u8);
+
+#[derive(Copy, Clone, PartialEq, PartialOrd, Ord, Eq)]
+enum Remainder {
+    E0, E1, E2, E3, E4, E5, E6, E7,
+}
+
+impl Remainder {
+    pub fn as_ticks(&self) -> usize {
+        match self {
+            &Remainder::E0 => 0,
+            &Remainder::E1 => 1,
+            &Remainder::E2 => 2,
+            &Remainder::E3 => 3,
+            &Remainder::E4 => 4,
+            &Remainder::E5 => 5,
+            &Remainder::E6 => 6,
+            &Remainder::E7 => 7,
+        }
+    }
+
+    pub fn from_ticks(ticks: usize) -> Self {
+        match ticks % 8 {
+            0 => Remainder::E0,
+            1 => Remainder::E1,
+            2 => Remainder::E2,
+            3 => Remainder::E3,
+            4 => Remainder::E4,
+            5 => Remainder::E5,
+            6 => Remainder::E6,
+            7 => Remainder::E7,
+            _ => unreachable!(),
+        }
+    }
+}
+
+struct TickSpan(usize, Remainder);
+
+impl TickSpan {
+    pub fn as_ticks(&self) -> usize {
+        (self.0 * 8) + self.1.as_ticks()
+    }
+
+    pub fn from_ticks(t: usize) -> Self {
+        Self(t / 8, Remainder::from_ticks(t))
+    }
+
+    pub fn from_num_chars(n: usize) -> Self {
+        Self(n, Remainder::E0)
+    }
+
+    pub fn chars_needed(&self) -> usize {
+        self.0 + if self.1.as_ticks() == 0 { 0 } else { 1 }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum BlockChar {
+    NL, R1, R2, R3, R4, R5, R6, R7, U1, U2, U3, U4, U5, U6, U7, FF,
+}
+
+impl<'a> From<&'a BlockChar> for &'static str {
+    fn from(bc: &'a BlockChar) -> Self {
+        match bc {
+            &BlockChar::NL => " ",
+            &BlockChar::R1 => "▏",
+            &BlockChar::R2 => "▎",
+            &BlockChar::R3 => "▍",
+            &BlockChar::R4 => "▌",
+            &BlockChar::R5 => "▋",
+            &BlockChar::R6 => "▊",
+            &BlockChar::R7 => "▉",
+            &BlockChar::U1 => "▁",
+            &BlockChar::U2 => "▂",
+            &BlockChar::U3 => "▃",
+            &BlockChar::U4 => "▄",
+            &BlockChar::U5 => "▅",
+            &BlockChar::U6 => "▆",
+            &BlockChar::U7 => "▇",
+            &BlockChar::FF => "█",
+        }
+    }
+}
+
+impl From<BlockChar> for &'static str {
+    fn from(bc: BlockChar) -> Self {
+        (&bc).into()
+    }
+}
+
+impl From<(Remainder, Direction)> for BlockChar {
+    fn from(t: (Remainder, Direction)) -> Self {
+        let (rem, dir) = t;
+
+        match (rem, dir) {
+            (Remainder::E0, Direction::Right) => BlockChar::NL,
+            (Remainder::E1, Direction::Right) => BlockChar::R1,
+            (Remainder::E2, Direction::Right) => BlockChar::R2,
+            (Remainder::E3, Direction::Right) => BlockChar::R3,
+            (Remainder::E4, Direction::Right) => BlockChar::R4,
+            (Remainder::E5, Direction::Right) => BlockChar::R5,
+            (Remainder::E6, Direction::Right) => BlockChar::R6,
+            (Remainder::E7, Direction::Right) => BlockChar::R7,
+            (Remainder::E0, Direction::Up) => BlockChar::NL,
+            (Remainder::E1, Direction::Up) => BlockChar::U1,
+            (Remainder::E2, Direction::Up) => BlockChar::U2,
+            (Remainder::E3, Direction::Up) => BlockChar::U3,
+            (Remainder::E4, Direction::Up) => BlockChar::U4,
+            (Remainder::E5, Direction::Up) => BlockChar::U5,
+            (Remainder::E6, Direction::Up) => BlockChar::U6,
+            (Remainder::E7, Direction::Up) => BlockChar::U7,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Direction { Right, Up, /* Left, Down, */ }
+
+#[derive(Copy, Clone)]
+enum TipSmoothing { PartialBlocks, Fade }
+
+struct GradientBlockLine(Vec<(BlockChar, ColorStyle)>);
+
+struct BlockLine(Vec<BlockChar>);
+
+impl BlockLine {
+    pub fn new(curr_blocks: usize, rem: Remainder, max_blocks: usize, direction: Direction) -> Self {
+        if curr_blocks >= max_blocks {
+            Self(vec![BlockChar::FF; max_blocks])
+        } else if curr_blocks == 0 && rem == Remainder::E0 {
+            Self(vec![BlockChar::NL; max_blocks])
+        } else {
+            let mut blocks = Vec::<BlockChar>::with_capacity(max_blocks);
+
+            for _ in 0..curr_blocks { blocks.push(BlockChar::FF); }
+            blocks.push(BlockChar::from((rem, Direction::Right)));
+            for _ in 0..(max_blocks - curr_blocks - 1) { blocks.push(BlockChar::NL); }
+
+            Self(blocks)
+        }
+    }
+}
+
+struct GradientBlockGen {
+    fg_color_a: RGB,
+    fg_color_b: Option<RGB>,
+    max_char_len: usize,
+    direction: Direction,
+}
+
+struct GradientBlockIter {
+    fg_color_a: RGB,
+    fg_color_b: RGB,
+    direction: Direction,
+    curr_ticks: usize,
+    max_blocks: usize,
+    tip_smoothing: TipSmoothing,
+}
+
+fn interpolate_rgb(rgb_a: RGB, rgb_b: RGB, curr: usize, max: usize) -> RGB {
+    let f = (curr as f64 / max as f64).clamp(0.0, 1.0);
+    (
+        ((1.0 - f) * rgb_a.0 as f64 + f * rgb_b.0 as f64).round() as u8,
+        ((1.0 - f) * rgb_a.1 as f64 + f * rgb_b.1 as f64).round() as u8,
+        ((1.0 - f) * rgb_a.2 as f64 + f * rgb_b.2 as f64).round() as u8,
+    )
+}
+
+struct GradientColorIter {
+    color_a: RGB,
+    color_b: RGB,
+    n: usize,
+    curr_step: usize,
+}
+
+impl GradientColorIter {
+    pub fn new(color_a: RGB, color_b: RGB, n: usize) -> Self {
+        Self {
+            color_a,
+            color_b,
+            n,
+            curr_step: 0,
+        }
+    }
+}
+
+impl Iterator for GradientColorIter {
+    type Item = RGB;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.curr_step < self.n {
+            let ret = Some(
+                if self.color_a != self.color_b && self.n > 1 {
+                    interpolate_rgb(self.color_a, self.color_b, self.curr_step, self.n - 1)
+                } else { self.color_a }
+            );
+            self.curr_step += 1;
+            ret
+        } else { None }
+    }
+}
+
+// impl Iterator for GradientBlockIter {
+//     type Item = GradientBlock;
+
+//     fn next(&mut self) -> Option<Self::Item> {
+//         let max_ticks = self.max_blocks * 8;
+
+//         if self.curr_ticks < max_ticks {
+//             let curr_blocks = (self.curr_ticks / 8) + if self.curr_ticks % 8 == 0 { 0 } else { 1 };
+
+//             // See if we need to interpolate a color gradient.
+//             let color_style =
+//                 if self.fg_color_a == self.fg_color_b {
+//                     ColorStyle{
+//                         front: ColorType::Color(Color::Rgb(self.fg_color_a.0, self.fg_color_a.1, self.fg_color_a.2)),
+//                         back: ColorType::Palette(PaletteColor::View),
+//                     }
+//                 }
+//                 else {
+//                     // Colors are always interpolated linearly, and by number of blocks, not by number of ticks.
+//                     let grad_color = interpolate_rgb(self.fg_color_a, self.fg_color_b, curr_blocks, self.max_blocks);
+//                     ColorStyle{
+//                         front: ColorType::Color(Color::Rgb(grad_color.0, grad_color.1, grad_color.2)),
+//                         back: ColorType::Palette(PaletteColor::View),
+//                     }
+//                 }
+//             ;
+//             self.curr_ticks += 1;
+//             None
+//         }
+//         else { None }
+//     }
+// }
+
+#[derive(Copy, Clone)]
+enum Easing {
+    Linear,
+    BounceOut,
+}
+
+impl Easing {
+    pub fn pos(&self, step: usize, max_steps: usize) -> usize {
+        let step = step.min(max_steps);
+
+        if max_steps == 0 { 0 }
+        else {
+            match self {
+                &Easing::Linear => step,
+                &Easing::BounceOut => {
+                    let t = step as f64 / max_steps as f64;
+                    const A: f64 = 7.5625;
+
+                    let (t_off, a_off) =
+                        if t < 1.0 / 2.75 { (0.0, 0.0) }
+                        else if t < 2.0 / 2.75 { (1.5 / 2.75, 0.75) }
+                        else if t < 2.5 / 2.75 { (2.25 / 2.75, 0.9375) }
+                        else { (2.625 / 2.75, 0.984375) }
+                    ;
+
+                    let tt = t - t_off;
+                    let f = (A * tt * tt) + a_off;
+
+                    (step as f64 * f).round() as usize
+                },
+            }
+        }
+    }
 }
 
 // fn ease_out_bounce(t: f64) -> f64 {
@@ -160,7 +433,7 @@ pub fn run() {
     // Build the UI from the model
     siv.add_layer(
         Dialog::around(
-            build_spectrum_view(Arc::clone(&model), Vec2::new(200, 1))
+            build_spectrum_view(Arc::clone(&model), Vec2::new(200, 2))
         )
     );
 
